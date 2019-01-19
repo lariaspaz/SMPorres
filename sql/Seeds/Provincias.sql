480477562e9062fe8401104249074e1206ff820c
Leo <leoariaspaz@gmail.com>
2019-01-19 14:28:47 -0300

    Se agregan scripts de carga de datos de Provincias y Departamentos

---
+++ sql/Seeds/Provincias.sql
@@ -0,0 +1,33 @@
+delete CursosAlumnos
+delete Alumnos
+delete Domicilios
+delete Barrios
+delete Localidades
+delete Departamentos
+delete Provincias
+go
+insert into Provincias(id, nombre) values( 1,'Santiago del Estero')
+insert into Provincias(id, nombre) values( 2,'Catamarca')
+insert into Provincias(id, nombre) values( 3,'Chaco')
+insert into Provincias(id, nombre) values( 4,'Chubut')
+insert into Provincias(id, nombre) values( 5,'Cordoba')
+insert into Provincias(id, nombre) values( 6,'Corrientes')
+insert into Provincias(id, nombre) values( 7,'Entre Rios')
+insert into Provincias(id, nombre) values( 8,'Formosa')
+insert into Provincias(id, nombre) values( 9,'Jujuy')
+insert into Provincias(id, nombre) values( 10,'La Pampa')
+insert into Provincias(id, nombre) values( 11,'La Rioja')
+insert into Provincias(id, nombre) values( 12,'Mendoza')
+insert into Provincias(id, nombre) values( 13,'Misiones')
+insert into Provincias(id, nombre) values( 14,'Neuquen')
+insert into Provincias(id, nombre) values( 15,'Rio Negro')
+insert into Provincias(id, nombre) values( 16,'Salta')
+insert into Provincias(id, nombre) values( 17,'San Juan')
+insert into Provincias(id, nombre) values( 18,'San Luis')
+insert into Provincias(id, nombre) values( 19,'Santa Cruz')
+insert into Provincias(id, nombre) values( 20,'Santa Fe')
+insert into Provincias(id, nombre) values( 21,'Buenos Aires')
+insert into Provincias(id, nombre) values( 22,'Tierra del Fuego')
+insert into Provincias(id, nombre) values( 23,'Tucumán')
+insert into Provincias(id, nombre) values( 24,'Capital Federal')
+
