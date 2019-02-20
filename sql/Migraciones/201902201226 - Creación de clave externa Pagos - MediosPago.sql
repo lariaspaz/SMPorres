0d36ca2096b3640dce946b738dc6ff2c57af1c64
Leonardo Arias Paz <leoariaspaz@gmail.com>
2019-02-20 15:43:59 -0300

    Se modifica el script de creación de tabla MediosPago: se incluye sus clave externa a Pagos.

--- sql/Migraciones/201902201226 - Creación de clave externa Pagos - MediosPago.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE [dbo].[Pagos]  WITH CHECK ADD  CONSTRAINT [FK_Pagos_MediosPago] FOREIGN KEY([IdMedioPago])
-REFERENCES [dbo].[MediosPago] ([Id])
-ON UPDATE CASCADE
-GO
-
-ALTER TABLE [dbo].[Pagos] CHECK CONSTRAINT [FK_Pagos_MediosPago]
-GO
