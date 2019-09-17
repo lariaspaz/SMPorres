21b7be6c9d37c4484a083678120a6eb77b0a65cc
Leo <leoariaspaz@gmail.com>
2019-09-17 12:35:33 -0300

    Formulario de planes de pago con control para el campo TipoBeca

---
+++ src/SMPorres/Models/TipoBeca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMPorres.Models
+{
+    public enum TipoBeca
+    {
+        /// <summary>
+        /// La beca se cobra hasta la fecha de vencimiento de la cuota.
+        /// </summary>
+        AplicaHastaVto = 1,
+
+        /// <summary>
+        /// La beca se cobra siempre, aún después de la fecha de vencimiento de la cuota.
+        /// </summary>
+        AplicaSiempre = 2
+    }
+}
