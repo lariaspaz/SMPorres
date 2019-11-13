697bf47304523fa45dbf096439a29d09e0a2431b
Leo <leoariaspaz@gmail.com>
2019-11-12 23:22:46 -0300

    Se agrega el proyecto BackupDB.

---
+++ src/BackupDB/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Controllers.DriveController();
+            int i = 0;
+            while (i < 3)
+            {
+                if (i > 0) System.Threading.Thread.Sleep(TimeSpan.FromMinutes(1));
+                c.SubirDB();
+                i++;
+            }
+        }
+    }
+}
