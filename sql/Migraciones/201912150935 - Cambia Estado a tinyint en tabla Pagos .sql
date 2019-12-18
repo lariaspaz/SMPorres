517af91aeb9709fa211366227bf1e94d060bf968
hernan_jhc <hernan.jhc@gmail.com>
2019-12-18 20:47:53 -0300

    Cambia tipo de datos smallint a tinyint. Marca pagadas a cuotas con montos, medio y fecha de pago registradas

--- sql/Migraciones/201912150935 - Cambia Estado a tinyint en tabla Pagos .sql
+++ sql/Migraciones/201912150935 - Cambia Estado a tinyint en tabla Pagos .sql
@@ -2,3 +2,15 @@ use smporres
 
 alter table pagos
 alter column Estado tinyint
+go
+
+update pagos
+set Estado = 3	--> Pagado
+from
+	pagos
+where
+	ImportePagado > 0 and
+	IdMedioPago > 0	and
+	FechaGrabacion >= '20190101'
+
+
