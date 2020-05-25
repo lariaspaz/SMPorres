b8fd86c6ca0b488fbf835afd92489dd87ee771b9
hernanjhc <hernan.jhc@gmail.com>
2020-05-25 12:05:28 -0300

    Asignar becas a Alumnos Ciclo Lectivo 2020 por Listado.-

---
+++ sql/Scripts/202005251156 - Asignar Becas 2020.sql
@@ -0,0 +1,236 @@
+--> Corrido en Producción 20200525
+
+drop table #listado
+go
+create table #listado(
+	IdCarrera		smallint null, 
+	NroDoc			numeric(8,0),
+	IdPlanPago		int, 
+	IdPago			int, 
+	IdBecaAlumno	int, 
+	IdAlumno		int
+)
+go
+-->Instrumentación Quirúrgica
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	42074694, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	41093996, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	38783365, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	32688672, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	33092789, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	41647089, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	41532815, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	42388208, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(1,	37411477, 0, 0, 0, 0)
+-->Radiología
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	39702856, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	40332636, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	42014754, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	43223720, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	39581206, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	43288329, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	36045504, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	37130284, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	33882267, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	39704304, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	40169726, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	43395385, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	42521630, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	42216211, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	42988947, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	40687726, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	42376098, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	38721316, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	41174056, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	40686942, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	35917393, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	42988678, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	39897781, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	37954900, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	39452348, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	40333716, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	4328886 , 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	43224159, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	34913213, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	33886383, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	43430321, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	42889539, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	40332627, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	41093759, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	44070150, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	40529131, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	36994281, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	43429815, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	42074430, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(2,	40050828, 0, 0, 0, 0)
+--> Hemoterapia
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	42650767, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	41480309, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	40604055, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	42348844, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	40827236, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	43430506, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	40050221, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	43159257, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	38484852, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	38733051, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(3,	36620623, 0, 0, 0, 0)
+-->Trabajador Social                , IdPlanPago, IdPago, IdBecaAlumno, IdAlumno                    , 0, 0, 0, 0
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	37314148, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	34694523, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	40445908, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	40899112, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	34183075, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	32859975, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	36928714, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	37700709, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	43623608, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	41362839, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	25114008, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	41648022, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	39702223, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	38641949, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	35086783, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	39056316, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(4,	42021044, 0, 0, 0, 0)
+-->Laboratorio
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	42521933, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	33627316, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	42521899, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	42267253, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	43497840, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	41937152, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	42929472, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	42628176, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	43361459, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	42889949, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	43159678, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	40169897, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	43496812, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	94490163, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	42947198, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	38368508, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	38640414, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	36133591, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	38509889, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	38735139, 0, 0, 0, 0)
+insert into #listado(IdCarrera, NroDoc, IdPlanPago, IdPago, IdBecaAlumno, IdAlumno) values(5,	43036107, 0, 0, 0, 0)
+
+--> Máximo ID BecasAlumnos
+select max(Id) from BecasAlumnos	--840
+
+--> 1 actualiza ID Alumnos
+update #listado
+set IdAlumno = a.Id
+from
+	#listado b, 
+	Alumnos a
+where
+	a.NroDocumento	= b.NroDoc
+--(96 filas afectadas de 98)
+
+/* Alumnos no encontrados
+
+select * from #listado where IdAlumno = 0
+IdCarrera, NroDoc,		IdPlanPago, IdPago, IdBecaAlumno, IdAlumno
+		2	4328886		0			0		0				0
+		5	38640414	0			0		0				0
+*/
+
+-->Actualiza plan de pagos
+update #listado
+set IdPlanPago	= pp.Id
+from
+	#listado b,
+	PlanesPago pp
+where
+	pp.IdAlumno	=	b.IdAlumno	and
+	pp.Estado	=	1			and--Estado Activo
+
+	pp.Id		= (select max(pp1.Id) 
+					from 
+						PlanesPago	pp1
+					where
+						pp1.IdAlumno	= pp.IdAlumno)
+--(96 filas afectadas)
+
+-->Obtener pagos	--> se esperan 854
+select 
+	l.IdCarrera,	l.NroDoc,	l.IdPlanPago, pago = p.Id,
+	l.IdBecaAlumno, l.IdAlumno
+from
+	#listado l,
+	Pagos p
+where
+	p.IdPlanPago	=	l.IdPlanPago	and
+	p.NroCuota		>	0				and
+	isnull(p.ImportePagado,0)	=	0
+--(719 filas afectadas)
+
+--> Con #becas actualizo IdPago
+drop table #becas
+go
+create table #becas(
+	IdCarrera		smallint null, 
+	NroDoc			numeric(8,0),
+	IdPlanPago		int, 
+	IdPago			int, 
+	IdBecaAlumno	int, 
+	IdAlumno		int
+)
+go
+insert into #becas(
+	IdCarrera,		NroDoc,		IdPlanPago,		IdPago,
+	IdBecaAlumno,	IdAlumno
+	)
+select 
+	l.IdCarrera,	l.NroDoc,	l.IdPlanPago, pago = p.Id,
+	l.IdBecaAlumno, l.IdAlumno
+from
+	#listado l,
+	Pagos p
+where
+	p.IdPlanPago	=	l.IdPlanPago	and
+	p.NroCuota		>	0				and
+	isnull(p.ImportePagado,0)	=	0
+--(719 filas afectadas)
+
+-->Calculo Id de tabla BecasAlumnos como datos de entrada a BecasAlumnos y Pagos
+drop table #temp
+go
+SELECT 
+	840 + ROW_NUMBER() 
+OVER(order by IdPago) as row_number,* 
+into #temp
+from 
+	#becas
+--(719 filas afectadas)
+
+select * from #temp
+
+--> Insertar BecasAlumnos	desde #temp
+begin tran becas
+
+insert into BecasAlumnos(
+	Id,	IdAlumno,	PorcentajeBeca
+)
+select 
+	t.row_number,	t.IdAlumno, PorcBeca=50
+from
+	#temp t
+--(719 filas afectadas)
+commit tran becas
+--rollback tran lap
+
+--> Actualizar IdBecaAlumno
+begin tran pagos
+
+update pagos 
+set IdBecaAlumno = t.row_number
+from
+	#temp t,
+	pagos p
+where
+	p.Id	= t.IdPago
+--(719 filas afectadas)
+commit tran pagos
+
