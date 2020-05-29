2ee029541fae1afe6b56efd90c9ae8566803f063
Leo <leoariaspaz@gmail.com>
2020-05-28 23:17:20 -0300

    Script para actualizar fechas de vencimientos de pagos.

---
+++ sql/Scripts/202005282144 - Actualiza fechas de vencimiento de los pagos desde los vencimientos de las cuotas.sql
@@ -0,0 +1,9 @@
+update Pagos
+set
+	FechaVto = c.VtoCuota
+from Pagos p
+inner join PlanesPago pp on pp.Id = p.Id
+inner join CursosAlumnos ca on ca.IdCurso = pp.IdCurso and ca.IdAlumno = pp.IdAlumno
+inner join Cuotas c on c.Cuota = p.NroCuota and c.CicloLectivo = ca.CicloLectivo
+where
+	p.FechaVto is null
