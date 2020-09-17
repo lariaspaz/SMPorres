ee13347acc39bdeffc6681750c2cb33bb6058da1
Leo <leoariaspaz@gmail.com>
2020-09-16 22:21:30 -0300

    Se quitan datos sensibles y configuraciones personalizadas por usuario.

--- src/BackupDB/Lib/DriveLibrary.cs
+++ src/BackupDB/Lib/DriveLibrary.cs
@@ -27,8 +27,8 @@ namespace BackupDB.Lib
 
             var secrets = new ClientSecrets
             {
-                ClientId = "1040777060117-bo0002q9hof8h6e43k5reo22aquj3cn7.apps.googleusercontent.com",
-                ClientSecret = "NCAVdbVz02ZbmKCb5MmtnsnY"
+                ClientId = "",
+                ClientSecret = ""
             };
 
             string[] scopes = new[] {
@@ -51,26 +51,6 @@ namespace BackupDB.Lib
             svc.HttpClient.Timeout = TimeSpan.FromMinutes(10);
 
             return svc;
-        }
-
-        internal static DriveService CrearServicioDriveComoCuentaServicio()
-        {
-            var fs = new System.IO.FileStream(@"C:\Proyectos\Varios\Google\Drive\backup-de-base-de-datos-7a63bd05a0be.json",
-                System.IO.FileMode.Open);
-            string[] scopes = new string[] { DriveService.Scope.Drive,
-                                             DriveService.Scope.DriveFile };
-            GoogleCredential credential = GoogleCredential.FromStream(fs).CreateScoped(scopes);
-
-            DriveService service = new DriveService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = "MyAppName",
-            });
-
-            //Long Operations like file uploads might timeout. 100 is just precautionary value, can be set to any reasonable value depending on what you use your service for.
-            //service.HttpClient.Timeout = TimeSpan.FromMinutes(100);
-
-            return service;
         }
     }
 }
