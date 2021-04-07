9de97bc5bf360ca5bb750e9efd29ed66caf06747
hernanjhc <hernan.jhc@gmail.com>
2021-04-06 23:13:21 -0300

    Scripts que se deben correr luego de descargar un backup en BD Consultas y BD SMPorres.

---
+++ sql/Scripts/202104062157 - Dar permisos usr_consultas.sql
@@ -0,0 +1,11 @@
+use Consultas
+go
+GRANT DELETE ON OBJECT::Consultas..TasasMoraWeb TO usr_consultas
+GRANT INSERT ON OBJECT::Consultas..TasasMoraWeb TO usr_consultas
+GRANT INSERT ON OBJECT::Consultas..AlumnosWeb TO usr_consultas
+GRANT INSERT ON OBJECT::Consultas..CursosAlumnosWeb TO usr_consultas
+GRANT INSERT ON OBJECT::Consultas..PagosWeb TO usr_consultas
+GRANT DELETE ON OBJECT::Consultas..PagosWeb TO usr_consultas
+GRANT UPDATE ON OBJECT::Consultas..AlumnosWeb TO usr_consultas
+GRANT UPDATE ON OBJECT::Consultas..CursosAlumnosWeb TO usr_consultas
+GRANT UPDATE ON OBJECT::Consultas..PagosWeb TO usr_consultas
---
+++ sql/Scripts/202104062157 - Dar permisos usr_inscripcion.sql
@@ -0,0 +1,6 @@
+use SMPorres
+go
+GRANT INSERT ON OBJECT::SMPorres..Alumnos TO usr_inscripcion
+GRANT INSERT ON OBJECT::SMPorres..CursosAlumnos TO usr_inscripcion
+GRANT INSERT ON OBJECT::SMPorres..PlanesPago TO usr_inscripcion
+GRANT INSERT ON OBJECT::SMPorres..Pagos TO usr_inscripcion
