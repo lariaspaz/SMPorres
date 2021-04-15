da69d4a55d581d647a5be48dc4679feaacb11a76
hernanjhc <hernan.jhc@gmail.com>
2021-04-15 19:10:20 -0300

    Se agregan permisos de select.

--- sql/Scripts/202104062157 - Dar permisos usr_inscripcion.sql
+++ sql/Scripts/202104062157 - Dar permisos usr_inscripcion.sql
@@ -1,5 +1,15 @@
 use SMPorres
 go
+GRANT SELECT ON OBJECT::SMPorres..Cuotas TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..Usuarios TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..PlanesPago TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..Pagos TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..Configuracion TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..CursosAlumnos TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..Cursos TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..Carreras TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..TiposDocumento TO usr_inscripcion
+GRANT SELECT ON OBJECT::SMPorres..Alumnos TO usr_inscripcion
 GRANT INSERT ON OBJECT::SMPorres..Alumnos TO usr_inscripcion
 GRANT INSERT ON OBJECT::SMPorres..CursosAlumnos TO usr_inscripcion
 GRANT INSERT ON OBJECT::SMPorres..PlanesPago TO usr_inscripcion
