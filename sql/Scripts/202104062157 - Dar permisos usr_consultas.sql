d27710ec00cc2f4202068643ad89d7c8cd7e550b
hernanjhc <hernan.jhc@gmail.com>
2021-05-14 18:17:10 -0300

    Agrega permisos de delete.

--- sql/Scripts/202104062157 - Dar permisos usr_consultas.sql
+++ sql/Scripts/202104062157 - Dar permisos usr_consultas.sql
@@ -6,6 +6,9 @@ GRANT INSERT ON OBJECT::Consultas..AlumnosWeb TO usr_consultas
 GRANT INSERT ON OBJECT::Consultas..CursosAlumnosWeb TO usr_consultas
 GRANT INSERT ON OBJECT::Consultas..PagosWeb TO usr_consultas
 GRANT DELETE ON OBJECT::Consultas..PagosWeb TO usr_consultas
+GRANT DELETE ON OBJECT::Consultas..CursosAlumnosWeb TO usr_consultas
+GRANT DELETE ON OBJECT::Consultas..AlumnosWeb TO usr_consultas
+GRANT DELETE ON OBJECT::Consultas..ConfiguracionWeb TO usr_consultas
 GRANT UPDATE ON OBJECT::Consultas..AlumnosWeb TO usr_consultas
 GRANT UPDATE ON OBJECT::Consultas..CursosAlumnosWeb TO usr_consultas
 GRANT UPDATE ON OBJECT::Consultas..PagosWeb TO usr_consultas
